4d0e20ec2ad5c1939bdf7eb37514dab0ce40834a
ahmedhh33 <ahmedhh3633@gmail.com>
2023-07-09 11:22:25 +0400

    Aa

---
+++ LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datastructure_linkedlist
+{
+    internal class LinkedList
+    {
+        private Node head;
+        public LinkedList() 
+        {
+            head = null;
+        }
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+            // 1. empty linkedlist. Head is null
+            if (head == null)
+            {
+                head = newNode;
+            }
+            //when items added to the end
+            else
+            {
+                Node currentNode = head;
+                while (currentNode.next != null)
+                {
+                    currentNode = currentNode.next;
+
+                }
+                currentNode.next = newNode;
+            }
+        }
+        public void display() 
+        {
+            Node node = head;
+            while (node != null)
+            {
+                Console.WriteLine(node.data);
+                node = node.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Datastructure_linkedlist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.Insert(10);
+            linkedList.Insert(20);
+            linkedList.Insert(30);
+
+            linkedList.display();
+        }
+    }
+}
